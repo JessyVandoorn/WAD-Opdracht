0c9a2945c6641dd4054bc515a0f6c3824cb8e945
Jessy Vandoorn <jessy.vandoorn@student.howest.be>
2019-05-10 18:21:00 +0100

    login screen added

--- client/duikerslog/src/App.js
+++ client/duikerslog/src/App.js
@@ -77,37 +77,8 @@ class App extends Component {
 
     return (
       <main className="main">
-        <div className="headerDiv">
-          <h1><Link to="/">Duikerslog</Link></h1>
-          <p><Link to="/Account">{authenticated? this.state.currentUser.email:"Account"}</Link></p>
-        </div>
-        <Navigation/>
-        <div>
-        <Query query={GET_ALL_DIVES}>
-          {
-            ({loading, error, data:{allDives}}) => {
-              if(loading) return <p>Loading ...</p>;
-              if(error) return <p>error: {error.message}</p>
-              return(
-          <Switch>
-            <ProtectedRoute path='/' userId={authId} exact component={OverviewDives} authenticated={authenticated} dives={allDives} /> 
-            <ProtectedRoute path='/DuikPlaatsen' component={DuikPlaatsen} exact authenticated={authenticated} userId={authId}  store={store}  />
-            <ProtectedRoute path='/Duikerslog' exact component={Duikerslog} authenticated={authenticated} userId={authId}  dives={allDives} history={history} />
-            <ProtectedRoute path="/Dive/add" component={AddDive} authenticated={authenticated} userId={authId} projects={allDives} events={store.evenementen} /> 
-            <Route path='/DuikPlaatsen/:id' render={({ match }) => {
-              const id = match.params.id;
-              return store.places[id]?<DivePlacesDetail key={id} id={id} store={store.places[id]} />:<NotFound />
-            }} /> 
-            <ProtectedRoute path='/DiveMaterial' exact component={DiveMaterial} authenticated={authenticated} userId={authId} />
-            <ProtectedRoute
-                        path="/Account"
-                        component={Account}
-                        authenticated={authenticated}
-                        userId={authId}
-                        projects={allDives}
-                        events={store.evenementen}
-                      />
-             <Route
+      {authId === "0" ? <div className="headerDiv"> 
+         <Route
                         path="/Register"
                         render={({ history }) => (
                           <Gate
@@ -140,13 +111,48 @@ class App extends Component {
 	                      />
 	                      )}
 	                    />
-            {/* <Route component={NotFound}/>  */}
-          </Switch>
-              )
-              }
-            }
-      </Query>
+        </div> : 
+        <div>  
+          <div className="headerDiv">
+        <h1><Link to="/">Duikerslog</Link></h1>
+        <p><Link to="/Account">{authenticated? this.state.currentUser.email:"Account"}</Link></p>
       </div>
+      <Navigation/>
+      <div>
+      <Query query={GET_ALL_DIVES}>
+        {
+          ({loading, error, data:{allDives}}) => {
+            if(loading) return <p>Loading ...</p>;
+            if(error) return <p>error: {error.message}</p>
+            return(
+        <Switch>
+          <ProtectedRoute path='/' userId={authId} exact component={OverviewDives} authenticated={authenticated} dives={allDives} /> 
+          <ProtectedRoute path='/DuikPlaatsen' component={DuikPlaatsen} exact authenticated={authenticated} userId={authId}  store={store}  />
+          <ProtectedRoute path='/Duikerslog' exact component={Duikerslog} authenticated={authenticated} userId={authId}  dives={allDives} history={history} />
+          <ProtectedRoute path="/Dive/add" component={AddDive} authenticated={authenticated} userId={authId} projects={allDives} events={store.evenementen} /> 
+          <Route path='/DuikPlaatsen/:id' render={({ match }) => {
+            const id = match.params.id;
+            return store.places[id]?<DivePlacesDetail key={id} id={id} store={store.places[id]} />:<NotFound />
+          }} /> 
+          <ProtectedRoute path='/DiveMaterial' exact component={DiveMaterial} authenticated={authenticated} userId={authId} />
+          <ProtectedRoute
+                      path="/Account"
+                      component={Account}
+                      authenticated={authenticated}
+                      userId={authId}
+                      projects={allDives}
+                      events={store.evenementen}
+                    />
+          {/* <Route component={NotFound}/>  */}
+        </Switch>
+            )
+            }
+          }
+    </Query>
+    </div></div>
+       }
+       
+        
       </main>
     );
   }
